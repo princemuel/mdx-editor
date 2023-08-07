4a042863e45824be33169c05f807340e2dbe5bb4
Prince Muel <vansomecsam@gmail.com>
2023-08-07 09:40:15 +0100

    ci: update vitest config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "happy-dom",
-    setupFiles: "./tests.config.ts",
+    setupFiles: "./src/utils/tests.setup.ts",
     // you might want to disable it, if you don't have tests that rely on CSS
     // since parsing CSS is slow
     // css: true,
