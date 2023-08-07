1523fcbcaa7a6feb2b2aaa3df39e31ff2bde34eb
Prince Muel <vansomecsam@gmail.com>
2023-08-07 08:48:18 +0100

    ci: setup vitest config
    Signed-off-by: Prince Muel <vansomecsam@gmail.com>

---
+++ vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: { alias: [{ find: "@/", replacement: "/src/" }] },
+
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: "./tests.config.ts",
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    // css: true,
+  },
+});
