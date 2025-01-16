7f309a7b0fd70c9cdede1a3f0c5ea22f02d78def
Prince Muel <vansomecsam@gmail.com>
2025-01-17 00:20:49 +0100

    chore: remove unnecessary files

--- src/components/atoms/greet/greet.tsx
+++
@@ -1,7 +0,0 @@
-interface Props {
-  name?: string;
-}
-
-export const Greet = (props: Props) => {
-  return <div>Hey {props.name ? props.name : "Guest"}</div>;
-};
--- src/components/atoms/index.ts
+++
@@ -1,2 +0,0 @@
-export * from "./greet";
-export * from "./input";
